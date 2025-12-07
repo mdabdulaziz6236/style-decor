78ebd8ba27990c91c4b69178771712b80043f851
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-12-07 14:51:53 +0600

    init: Setup Router and installed tailwindcss

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            In the name of Allah Hafez. 
+            Salli alal Habib wa barik wa sallim Wa aihi wa ashabihi ajmain.
+        </div>
+    );
+};
+
+export default Home;
