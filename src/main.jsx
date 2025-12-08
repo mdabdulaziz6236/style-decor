8ddf03a461974dc511d0b4ff1d4d5ec1aee99d06
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-12-08 10:29:39 +0600

    feat: installed tanstack/reat-query and updated useRole

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { router } from "./routes/Routes";
 import { RouterProvider } from "react-router/dom";
 import AuthProvider from "./providers/AuthProvider";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
